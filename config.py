e1cbf1f37476896a3b536fcd79652bcbd513ca9b
Ment_Niu <ment_niu@163.com>
2019-07-29 10:17:39 +0800

    Create config.py
    add config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+#coding=utf-8 
+# interval
+interval = 60 # 上报的 step 间隔
+
+# vcenter
+host = "172.16.10.127" # vcenter 的地址
+user = "administrator@vsphere.local" # vcenter 的用户名
+pwd = "P@ssw0rd" # vcenter 的密码
+port = 443 # vcenter 的端口
